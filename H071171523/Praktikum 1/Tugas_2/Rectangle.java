7b9811883a7e7f4627e14ffad4e015bfc520e328
nuwechi35 <nurwesirinaw@gmail.com>
2020-03-07 20:38:51 +0800

    Praktikum 1 & 2 H071171523

---
+++ H071171523/Praktikum 1/Tugas_2/Rectangle.java
@@ -0,0 +1,13 @@
+class Rectangle {
+    double height;
+    double weight;
+    
+    // public Rectangle(double height, double weight){
+    //     this.height = height;
+    //     this.weight = weight;
+    // }
+    
+    double getArea(){
+        return height*weight;
+    }
+}
---
+++ H071171523/Praktikum 1/Tugas_2/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args){
+        Rectangle rectangle = new Rectangle();
+        rectangle.weight = 50;
+        rectangle.height = 3;
+        System.out.printf("Luas = "+rectangle.getArea());
+    }
+}
