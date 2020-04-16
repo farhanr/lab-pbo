116e8c914741322ee6db2b51091e052480e94df8
trulyroselyne <cadanganuly@gmail.com>
2020-04-16 22:19:21 +0800

    Polymorphism

---
+++ H071191034/PRAKTIKUM-5/Converter.java
@@ -0,0 +1,7 @@
+abstract class Converter{
+    
+    public abstract void start();
+    public abstract void stop();
+
+}
+
---
+++ H071191034/PRAKTIKUM-5/Data.java
@@ -0,0 +1,7 @@
+
+public interface Data {
+
+    long teraToGiga(long value);
+    long teraToMega(long value);
+    long teraToKilo(long value);
+}
